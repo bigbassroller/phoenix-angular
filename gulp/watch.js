00524a0d8729a7fbaff17ac2626b471264343c73
Michael Chavez <bigbassroller@gmail.com>
2015-12-09 09:23:58 -0800

    added gulp files

---
+++ gulp/watch.js
@@ -0,0 +1,10 @@
+var Gulp = require('gulp');
+
+
+Gulp.task('watch', function () {
+
+    global.isWatching = true;
+    Gulp.watch('./client/*.scss', ['sass']);
+    Gulp.watch('./client/*/*.scss', ['sass']);
+    Gulp.watch('./client/*/*/*.scss', ['sass']);
+});
